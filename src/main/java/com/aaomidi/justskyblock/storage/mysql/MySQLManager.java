4281d261c832b42044e3e9931f0edb57a2d22dac
Amir <amir+bitbucket@aaomidi.com>
2014-09-17 00:05:21 +0330

    Added a nice query.

--- src/main/java/com/aaomidi/justskyblock/storage/mysql/MySQLManager.java
+++ src/main/java/com/aaomidi/justskyblock/storage/mysql/MySQLManager.java
@@ -1,7 +1,22 @@
 package com.aaomidi.justskyblock.storage.mysql;
 
+import com.aaomidi.justskyblock.JustSkyblock;
+
 /**
  * @author aaomidi
  */
 public class MySQLManager {
+    private final JustSkyblock instance;
+    private final MySQLConnector mySQLConnector;
+
+    public MySQLManager(JustSkyblock instance) {
+        this.instance = instance;
+        //TODO: Actually make this point to something :P
+        this.mySQLConnector = null;
+    }
+
+    public void initTables() {
+        //TODO: Mark you write these, you know how to make more efficient queries. :P
+        String query = "";
+    }
 }

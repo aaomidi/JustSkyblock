ebea87ed0dd51ca6e035b8eff03220bfe93cfddb
Amir <amir+bitbucket@aaomidi.com>
2014-09-16 16:31:33 +0330

    Made the API more descriptive.

--- src/main/java/com/aaomidi/justskyblock/api/API.java
+++ src/main/java/com/aaomidi/justskyblock/api/API.java
@@ -1,52 +1,25 @@
 package com.aaomidi.justskyblock.api;
 
 import com.aaomidi.justskyblock.JustSkyblock;
-import com.aaomidi.justskyblock.api.objects.SkyblockCommand;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
-
-import java.util.HashMap;
+import com.aaomidi.justskyblock.api.command.SkyblockCommandAPI;
+import lombok.Getter;
 
 /**
  * @author aaomidi
  */
-public class API implements CommandExecutor {
+public class API {
     private final JustSkyblock instance;
-    private final HashMap<String, SkyblockCommand> registeredCommands;
+    @Getter
+    private SkyblockCommandAPI skyblockCommandAPI;
 
     public API(JustSkyblock instance) {
         this.instance = instance;
-        registeredCommands = new HashMap<>();
-        instance.getCommand("is").setExecutor(this);
+        this.init();
     }
 
-    public void registerCommand(SkyblockCommand skyblockCommand) {
-        registeredCommands.put(skyblockCommand.getCommandLabel(), skyblockCommand);
-        if (skyblockCommand.getAliases() == null || skyblockCommand.getAliases().length == 0) {
-            return;
-        }
-        for (String alias : skyblockCommand.getAliases()) {
-            registeredCommands.put(alias, skyblockCommand);
-        }
+    private void init() {
+        skyblockCommandAPI = new SkyblockCommandAPI(instance);
     }
 
-    @Override
-    public boolean onCommand(CommandSender commandSender, Command cmd, String commandLabel, String[] args) {
-        if (cmd.getName().equalsIgnoreCase("is")) {
-            if (args.length == 0) {
-                // Send information about all the registered commands.
-                return true;
-            }
-            String subCommand = args[0];
-            if (!registeredCommands.containsKey(subCommand)) {
-                //Send command not found.
-                return true;
-            }
-            SkyblockCommand skyblockCommand = registeredCommands.get(subCommand);
 
-
-        }
-        return true;
-    }
 }

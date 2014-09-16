0e2c5efdc81685697b773d2f985a3a83decdc8fa
Amir <amir+bitbucket@aaomidi.com>
2014-09-16 21:22:28 +0330

    Island generation somewhat done?

--- src/main/java/com/aaomidi/justskyblock/engine/generator/IslandGeneration.java
+++ src/main/java/com/aaomidi/justskyblock/engine/generator/IslandGeneration.java
@@ -1,6 +1,7 @@
 package com.aaomidi.justskyblock.engine.generator;
 
 import com.aaomidi.justskyblock.JustSkyblock;
+import com.sk89q.worldedit.CuboidClipboard;
 import com.sk89q.worldedit.EditSession;
 import com.sk89q.worldedit.blocks.BaseBlock;
 import com.sk89q.worldedit.regions.Region;
@@ -8,6 +9,8 @@ import lombok.RequiredArgsConstructor;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.util.Vector;
+
+import java.io.File;
 
 
 /**
@@ -18,14 +21,20 @@ public class IslandGeneration {
     private final JustSkyblock instance;
     private final BaseBlock AIR = new BaseBlock(Material.AIR.getId());
 
-    private void buildIsland(Vector loc1, Vector loc2, World world) {
+    // TODO: Re-Write so it doesn't try to spawn everything at the same time.
+    private void buildIsland(Vector loc1, Vector loc2, World world, File file) {
         try {
             EditSession editSession = instance.getEngineManager().getWorldEditUtils().getEditSession(world);
             editSession.enableQueue();
             Region region = LocationsParser.getRegion(loc1, loc2);
             // Replacing the blocks is MUCH faster than just setting them all to air.
             editSession.replaceBlocks(region, null, AIR);
+            editSession.flushQueue();
             // Loading schematics.
+            CuboidClipboard cuboidClipboard = instance.getEngineManager().getWorldEditUtils().getCuboidClipboard(file);
+            // Pasting the schematic.
+            cuboidClipboard.paste(editSession, region.getCenter(), false);
+            editSession.flushQueue();
             // Cause pokemon is awesome.
         } catch (Exception ex) {
             throw new RuntimeException("Problem with generating island.", ex);
--- src/main/java/com/aaomidi/justskyblock/engine/generator/WorldEditUtils.java
+++ src/main/java/com/aaomidi/justskyblock/engine/generator/WorldEditUtils.java
@@ -1,8 +1,12 @@
 package com.aaomidi.justskyblock.engine.generator;
 
 import com.aaomidi.justskyblock.JustSkyblock;
+import com.sk89q.worldedit.CuboidClipboard;
 import com.sk89q.worldedit.EditSession;
+import com.sk89q.worldedit.schematic.SchematicFormat;
 import org.bukkit.World;
+
+import java.io.File;
 
 /**
  * @author aaomidi
@@ -17,5 +21,14 @@ public class WorldEditUtils {
     protected EditSession getEditSession(World world) {
         return instance.getWorldEditPlugin().getWorldEdit().getEditSessionFactory().getEditSession(LocationsParser.getWorldEditWorld(world), -1);
     }
+
+    protected SchematicFormat getSchematicFormat(File file) {
+        return SchematicFormat.getFormat(file);
+    }
+
+    protected CuboidClipboard getCuboidClipboard(File file) throws Exception {
+        return getSchematicFormat(file).load(file);
+    }
+
 
 }

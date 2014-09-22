2d8897b23e75e13356b685c567aaf3e6e82129e2
Amir <amir+bitbucket@aaomidi.com>
2014-09-22 19:16:12 +0330

    I \dont\ know how to do this one :P

--- src/main/java/com/aaomidi/justskyblock/storage/StorageManagement.java
+++ src/main/java/com/aaomidi/justskyblock/storage/StorageManagement.java
@@ -1,7 +1,21 @@
 package com.aaomidi.justskyblock.storage;
 
+import com.aaomidi.justskyblock.JustSkyblock;
+import com.aaomidi.justskyblock.engine.objects.SkyblockPlayer;
+
+import java.util.concurrent.ConcurrentMap;
+
 /**
  * @author aaomidi
  */
 public class StorageManagement {
+    private static ConcurrentMap<String, SkyblockPlayer> skyblockPlayers;
+    private static DataStorageInterface data;
+    private final JustSkyblock instance;
+
+    public StorageManagement(JustSkyblock instance) {
+        this.instance = instance;
+        //TODO: Get the data storage type.
+        data = null;
+    }
 }

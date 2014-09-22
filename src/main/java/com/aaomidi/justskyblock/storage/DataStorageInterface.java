4b4a410307759260ce8e00bbe450c459e9d76be3
Amir <amir+bitbucket@aaomidi.com>
2014-09-22 18:45:15 +0330

    Added SkyblockIsland's Getters

--- src/main/java/com/aaomidi/justskyblock/storage/DataStorageInterface.java
+++ src/main/java/com/aaomidi/justskyblock/storage/DataStorageInterface.java
@@ -2,13 +2,57 @@ package com.aaomidi.justskyblock.storage;
 
 import com.aaomidi.justskyblock.engine.objects.SkyblockIsland;
 import com.aaomidi.justskyblock.engine.objects.SkyblockPlayer;
+import org.bukkit.util.Vector;
+
+import java.util.UUID;
 
 /**
  * @author aaomidi
  */
+//TODO: Finish the damned javadocs :P
 public abstract class DataStorageInterface {
-
+    /**
+     * Gets the skyblock player.
+     *
+     * @param playerName Name of player.
+     * @return SkyblockPlayer object.
+     */
     public abstract SkyblockPlayer getSkyblockPlayer(String playerName);
 
-    public abstract SkyblockIsland getSkyblockIsland();
+    /**
+     * Gets the skyblock island.
+     *
+     * @param uuid Unique ID of player.
+     * @return SkyblockPlayer object.
+     */
+    public abstract SkyblockPlayer getSkyblockPlayer(UUID uuid);
+
+    /**
+     * Gets the skyblock island.
+     *
+     * @param playerName Name of player.
+     * @return SkyblockIsland object.
+     */
+    public abstract SkyblockIsland getSkyblockIsland(String playerName);
+
+    /**
+     * Gets the skyblock island.
+     *
+     * @param uuid Unique ID of player.
+     * @return SkyblockIsland object.
+     */
+    public abstract SkyblockIsland getSkyblockIsland(UUID uuid);
+
+    public abstract boolean createSkyblockPlayer(String playerName);
+
+    public abstract boolean createSkyblockPlayer(UUID uuid);
+
+    public abstract boolean createSkyblockIsland(String playerName, String serverName, Vector min, Vector max);
+
+    public abstract boolean createSkyblockIsland(UUID uuid, String serverName, Vector min, Vector max);
+
+    public abstract boolean updateSkyblockPlayer(SkyblockPlayer skyblockPlayer);
+
+    public abstract boolean updateSkyblockIsland(SkyblockIsland skyblockIsland);
+
 }
